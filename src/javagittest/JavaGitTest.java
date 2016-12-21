11363f48bbed3a0583ebf8ed420f296d936b7903
SuperSecretNooneWillFind <SuperSecretNooneWillFind.noreply@github.com>
2016-12-21 01:18:58 -0800

    Super man
    the awesome adventures of superman

---
+++ src/javagittest/JavaGitTest.java
@@ -0,0 +1,10 @@
+package javagittest;
+
+import static javagittest.NewClass.greet;
+
+public class JavaGitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        greet("Bob");
+    }
+}
---
+++ src/javagittest/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagittest;
+
+/**
+ *
+ * @author KoolKevin
+ */
+public class NewClass {
+    static void greet(String name){
+        System.out.println("Hello " + name);
+    }
+}
